7cad7aa4174c636362e33313ebad0390d6ef60f4
Nicamoyanof <nicamoyanofunes98@gmail.com>
2025-12-04 19:26:16 -0300

    agrego actions

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   // apiBaseUrl: 'http://localhost:4100',
-  apiBaseUrl: 'http://72.60.138.13:10000',
+  apiBaseUrl: 'https://api.mesero.online',
   audience: 'https://dev-w1bl5owcn51l2yqv.us.auth0.com/api/v2/',
   auth0ClientId: 'pK4TB4UnEwFB87YBtGtoASYk6nQO5XD1',
   production: false,
